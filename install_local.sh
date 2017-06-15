eda9742604b50cdcabb36a8234fc352c3b94061c
nanotumblr <peropoyi@gmail.com>
2017-06-15 17:25:48 +0900

    add git status sh

--- install_local.sh
+++ install_local.sh
@@ -9,3 +9,7 @@ cp local/.zshrc.local ~/
 if ! grep "source ~/.zshrc.local" ~/.zshrc >/dev/null 2>&1; then
   echo "source ~/.zshrc.local" >> ~/.zshrc
   fi
+
+mkdir ~/dotfiles
+mkdir ~/dotfiles/bin
+cp local/git-echo-branch-tmux-current-pane ~/dotfiles/bin
